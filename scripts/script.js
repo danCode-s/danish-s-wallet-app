be4b4aabe39d11aa64b99eaa1ba6fc0d1ea35bfe
danCode-s <dancodes9@gmail.com>
2023-12-11 14:25:45 +0500

    some CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,35 @@
-let userData = [];
-saveLoginCode();
+let userData = [
+    loginCode = {
+        name: 'User',
+        user_name: 'user12',
+    }
+];
+// saveLoginCode();
 function generateLoginCode(){
-    //userData = [];
-    saveLoginCode();
+    userData = [];
+    // saveLoginCode();
     const randomNumber = Math.random();
     const loginCode = Math.round(randomNumber * 900 + 100);
 
     
-    userData.push({
+    userData[0] = {
         loginCode,
-    })
+    };
+    
 
-    localStorage.setItem('code', JSON.stringify(userData))
+    // localStorage.setItem('code', JSON.stringify(userData))
     console.log(userData);
     
 };
 // generateLoginCode();
 
-function saveLoginCode(){
-    userData = JSON.parse(localStorage.getItem('code'));
-}
+// function saveLoginCode(){
+//     userData = JSON.parse(localStorage.getItem('code'));
+// }
 
 document.querySelector('.generate-btn')
     .addEventListener('click', () => {
         generateLoginCode();
         document.querySelector('.login-code-demo')
-            .innerHTML = `<p>${userData[0].loginCode}</p>`;
+            .innerHTML = `<p>${userData[0]}</p>`;
     })
