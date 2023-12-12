202bf8131baf8d007a46dad7778deaee0817214e
danCode-s <dancodes9@gmail.com>
2023-12-12 17:00:43 +0500

    remove username field and some JS add

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+let userData = [];
+let allCodes = [];
 
-// saveLoginCode();
+
 function generateLoginCode(){
     const randomNumber = Math.random();
     const loginCode = Math.round(randomNumber * 900 + 100);
@@ -8,6 +10,24 @@ function generateLoginCode(){
 };
 
 
+document.querySelector('.new-generate-btn')
+.addEventListener('click', () => {
+    const firstName = document.querySelector('.js-name-input').value;
+    const user_name = document.querySelector('.js-user-name-input').value;
+    let code;
+    if(firstName){
+        code = generateLoginCode();
+        allCodes.push(generateLoginCode());
+
+    } else {
+        alert('enter valid input')
+    }
+    console.log(allCodes);
+    
+
+    }
+    
+    )
 
 
 
