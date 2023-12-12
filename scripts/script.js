08c1e2f132df0fea203c43b963d5205d20acfe13
danCode-s <dancodes9@gmail.com>
2023-12-12 16:10:38 +0500

    some changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,10 @@
-let userData = [
-    loginCode = {
-        name: 'User',
-        user_name: 'user12',
-    }
-];
+
 // saveLoginCode();
 function generateLoginCode(){
-    userData = [];
-    // saveLoginCode();
     const randomNumber = Math.random();
     const loginCode = Math.round(randomNumber * 900 + 100);
 
-    
-    userData[0] = {
-        loginCode,
-    };
-
-
-    // localStorage.setItem('code', JSON.stringify(userData))
-    console.log(userData);
-    
+    return loginCode;
 };
 
 
