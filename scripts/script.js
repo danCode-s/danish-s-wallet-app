710dc05610cbd350153b7e71b6981073524d4de8
danCode-s <dancodes9@gmail.com>
2023-12-13 15:13:31 +0500

    saving userdata now

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,92 @@
-let userData = [];
+let fullUser;
 getSavedData();
-let allCodes = [];
-
+let user_name;
+let code;
+let loginCode;
 function getSavedData() {
     const savedData = localStorage.getItem('user-data');
     if (savedData) {
-        userData = JSON.parse(savedData);
+        fullUser = JSON.parse(savedData);
     } else {
-        userData = [];
+        fullUser = {};
     }
 };
 
 function saveDataToLocalStorage() {
-    localStorage.setItem('user-data', JSON.stringify(userData));
+    localStorage.setItem('user-data', JSON.stringify(fullUser));
 };
 function generateLoginCode(){
     const randomNumber = Math.random();
-    const loginCode = Math.round(randomNumber * 900 + 100);
+    loginCode = Math.round(randomNumber * 900 + 100);
     return loginCode;
     
 };
 
 
-document.querySelector('.new-generate-btn')
-    .addEventListener('click', () => {
+function submit(){
     getSavedData();
-    const firstName = document.querySelector('.js-name-input').value;
-    //const user_name = document.querySelector('.js-user-name-input').value;
-    let code;
-    if(firstName){
-        code = generateLoginCode();
-        allCodes.push(code);
-        userData[code] = {
-            firstName: firstName,
-        }
-        
-        document.querySelector('.changeable')
-            .innerHTML = `<p class='welcome-msg'>Welcome, ${userData[code].firstName} 👋<br>Your Login Code is <span class="span">${code}</span></p>`;
+    if(document.querySelector('.submit-btn')){
+        console.log(fullUser);
+        document.querySelector('.submit-btn')
+         .addEventListener('click', () =>{
+                const enteredCode = document.querySelector('.input-code').value;
+                for (const key in fullUser[user_name].loginCode){
+                    console.log(key);
+                    if (key === enteredCode){
+                        document.querySelector('.test')
+                            .innerHTML = `<p>${enteredCode}</p>`;
+                                    
+                    }
+                }
+                    
+            })
+        }    
+         
+}
+
+
+  
+function generate(){
+    if(document.querySelector('.small-wrapper')){
         document.querySelector('.new-generate-btn')
-            .innerHTML = `<a href="index.html">Login Page</a>`;
+            .addEventListener('click', () => {
+            getSavedData();
+            const firstName = document.querySelector('.js-name-input').value;
+            user_name = document.querySelector('.js-user-name-input').value;
+            
+            
+            if(firstName && user_name){
+                code = generateLoginCode();
+                fullUser = {
+                    [user_name]: {
+                        name: firstName,
+                        loginCode: code,
+                    }
+                }
+                console.log(fullUser); 
+                
+                saveDataToLocalStorage();
+                document.querySelector('.changeable')
+                    .innerHTML = `<p class='welcome-msg'>Welcome, ${fullUser[user_name].name} 👋<br>Your Login Code is <span class="span">${code}</span></p>`;
+                document.querySelector('.new-generate-btn')
+                    .innerHTML = `<a href="index.html">Login Page</a>`;
+                
+            } else {
+                alert('enter valid input')
+            }
         
-    } else {
-        alert('enter valid input')
-    }
-    // localStorage.setItem('user-data', JSON.stringify(userData[code]));
-    console.log(allCodes);
-    console.log(userData);
+            
+            
+        
     
-
+        });
     }
-);
+    
+    
+    
+}
 
-document.querySelector('.submit-btn')
-.addEventListener('click', () =>{
-        const enteredCode = document.querySelector('.input-code').value || 101;
-        document.querySelector('.test')
-            .innerHTML = `<p>${enteredCode}</p>`;
-    })
-
-
-            
-            
-
-             
+generate();
+submit();   
             
         
